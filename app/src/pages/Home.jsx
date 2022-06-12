3ff1a77d1b61958b03dd5d364e2eac2f71e67840
b-go-bell <bridget.go.bell@gmail.com>
2022-06-12 14:55:46 -0600

    color detaisl

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -3,10 +3,32 @@ import Nav from './../components/Nav.jsx';
 import Crossword from './../components/home/Crossword.jsx';
 
 import mail from './../resources/media/home/mail.png';
+import email from './../resources/media/home/emerald-mail.png';
 import linkedin from './../resources/media/home/linkedin.png';
+import elinkedin from './../resources/media/home/emerald-linkedin.png';
 import github from './../resources/media/home/github.png';
+import egithub from './../resources/media/home/emerald-github.png';
 
 function Home() {
+
+  function hover(id) {
+    if(id === 'mail')
+      document.getElementById(id).src = email;
+    if(id === 'github')
+      document.getElementById(id).src = egithub;
+    if(id === 'linkedin')
+      document.getElementById(id).src = elinkedin;
+  }
+
+  function unhover(id) {
+    if(id === 'mail')
+      document.getElementById(id).src = mail;
+    if(id === 'github')
+      document.getElementById(id).src = github;
+    if(id === 'linkedin')
+      document.getElementById(id).src = linkedin;
+  }
+
   return (
     <div className="home-container">
       <Nav/>
@@ -33,14 +55,14 @@ function Home() {
           </div>
           <br/>
           <div className="external-links">
-              <a href="mailto:bridget.go.bell@gmail.com">
-                <img src={mail} className="icon" alt="mail"/>
+              <a href="mailto:bridget.go.bell@gmail.com" className="mail-icon">
+                <img id="mail" src={mail} className="icon"  alt="mail" onMouseOver={ () => hover("mail")} onMouseOut={() => unhover("mail")} />
               </a>
               <a href="https://github.com/b-go-bell" target="_blank" rel="noopener noreferrer">
-                <img src={github} className="icon" alt="github"/>
+                <img id="github" src={github} className="icon" alt="github" onMouseOver={ () => hover("github")} onMouseOut={() => unhover("github")} />
               </a>
               <a href="https://www.linkedin.com/in/bridget-go-bell/" target="_blank" rel="noopener noreferrer">
-                <img src={linkedin} className="icon" alt="linkedin"/>
+                <img id="linkedin" src={linkedin} className="icon" alt="linkedin" onMouseOver={ () => hover("linkedin")} onMouseOut={() => unhover("linkedin")}/>
               </a>
           </div>
           <br/>
